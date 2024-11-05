d13c908dacd0d34d8f150cae9ee3e82bf3c22260
Sangwon Lee <sang4939@gmail.com>
2024-11-05 16:53:50 +0900

    Initial commit

---
+++ src/main/java/org/sunbong/allmart_api/AllmartApiApplication.java
@@ -0,0 +1,13 @@
+package org.sunbong.allmart_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllmartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllmartApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sunbong/allmart_api/AllmartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sunbong.allmart_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllmartApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
