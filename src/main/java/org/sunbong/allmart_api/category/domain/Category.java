21474be119cbab67c006cc3b3abd55ff14abef7a
hajaemin <98006326+ha-jae-min@users.noreply.github.com>
2024-11-06 14:23:08 +0900

    buildgradle (#14)
    * feat: phoneNumber 제거
    
    * feat: buildgralde 추가

---
+++ src/main/java/org/sunbong/allmart_api/category/domain/Category.java
@@ -0,0 +1,22 @@
+package org.sunbong.allmart_api.category.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_category")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryID;
+
+    @Column(length = 100)
+    private String name;
+
+}
