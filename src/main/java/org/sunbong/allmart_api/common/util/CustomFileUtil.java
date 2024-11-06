a271c3ca1f64e8b587f4af2565cdc6874076765f
hajaemin <woals3527@naver.com>
2024-11-06 16:57:15 +0900

    feat: 커스텀파일유틸

---
+++ src/main/java/org/sunbong/allmart_api/common/util/CustomFileUtil.java
@@ -0,0 +1,132 @@
+package org.sunbong.allmart_api.common.util;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class CustomFileUtil {
+
+    @Value("${org.sunbong.upload.path}")
+    private String uploadPath;
+
+    @PostConstruct
+    public void init() {
+        File tempFolder = new File(uploadPath);
+
+        if(tempFolder.exists() == false) {
+            tempFolder.mkdir();
+        }
+
+        uploadPath = tempFolder.getAbsolutePath();
+
+        log.info("-------------------------------------");
+        log.info(uploadPath);
+    }
+
+    public List<String> saveFiles(List<MultipartFile> files)throws RuntimeException{
+
+        if(files == null || files.size() == 0){
+            return List.of();
+        }
+
+        List<String> uploadNames = new ArrayList<>();
+
+        for (MultipartFile multipartFile : files) {
+
+            String savedName = UUID.randomUUID().toString() + "_" + multipartFile.getOriginalFilename();
+
+            Path savePath = Paths.get(uploadPath, savedName);
+//            log.info("실제 경로 : " + savePath);
+            try {
+                Files.copy(multipartFile.getInputStream(), savePath);
+
+                String contentType = multipartFile.getContentType();
+
+                log.info("-------------------------");
+                log.info("contentType : " + contentType);
+                log.info("-------------------------");
+
+                if(contentType != null && contentType.startsWith("image")){ //이미지여부 확인
+
+                    Path thumbnailPath = Paths.get(uploadPath, "s_"+savedName);
+
+                    Thumbnails.of(savePath.toFile())
+                            .size(400,400)
+                            .toFile(thumbnailPath.toFile());
+                }
+
+                uploadNames.add(savedName);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }//end for
+        return uploadNames;
+    }
+
+    public ResponseEntity<Resource> getFile(String fileName) {
+
+        Resource resource = new FileSystemResource(uploadPath+ File.separator + fileName);
+
+        if(!resource.exists()) {
+
+            resource = new FileSystemResource(uploadPath+ File.separator + "default.png");
+
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try{
+            headers.add("Content-Type", Files.probeContentType( resource.getFile().toPath() ));
+        } catch(Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+
+    public void deleteFiles(List<String> fileNames) {
+
+        if(fileNames == null || fileNames.size() == 0){
+            return;
+        }
+
+        fileNames.forEach(fileName -> {
+
+            //썸네일이 있는지 확인하고 삭제
+            String thumbnailFileName = "s_" + fileName;
+            Path thumbnailPath = Paths.get(uploadPath, thumbnailFileName);
+            Path filePath = Paths.get(uploadPath, fileName);
+
+            try {
+                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(thumbnailPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+    }
+
+
+
+}
