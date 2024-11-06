bba70cdbc59c8bd4da19cc57c3d0165235c3a7d6
youngin <duddls1304@naver.com>
2024-11-06 17:02:51 +0900

    payment 완료직전

--- src/main/java/org/sunbong/allmart_api/payment/repository/PaymentRepository.java
+++ src/main/java/org/sunbong/allmart_api/payment/repository/PaymentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.sunbong.allmart_api.payment.domain.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
 }
