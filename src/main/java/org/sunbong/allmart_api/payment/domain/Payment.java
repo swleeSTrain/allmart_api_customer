b7fcf9a0434e09acd15c787e55749a720d536433
youngin <duddls1304@naver.com>
2024-11-06 13:20:23 +0900

    payment엔티티

---
+++ src/main/java/org/sunbong/allmart_api/payment/domain/Payment.java
@@ -0,0 +1,34 @@
+package org.sunbong.allmart_api.payment.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sunbong.allmart_api.common.domain.BaseEntity;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT")
+    private long paymentID;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod method;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal amount; // BigDecimal을 그대로 사용
+
+    @Column(nullable = false, columnDefinition = "int default 0") // 기본값을 명확히 설정하기 위해
+    private int completed = 0; // 기본값을 0으로 설정
+}
---
+++ src/main/java/org/sunbong/allmart_api/payment/domain/PaymentMethod.java
@@ -0,0 +1,5 @@
+package org.sunbong.allmart_api.payment.domain;
+
+public enum PaymentMethod {
+    MEET_WITH_CARD, MEET_WITH_CASH, ONLINE_PAYMENT
+}
