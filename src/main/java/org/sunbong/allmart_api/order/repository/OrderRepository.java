8605dde7499e9ecf3141e881d8c7cdbb80e86025
Daejin Kim <sp04212016@gmail.com>
2024-11-06 16:50:45 +0900

    feature: Order쪽 매장관리자 리스트 qureydsl 개발중.

---
+++ src/main/java/org/sunbong/allmart_api/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package org.sunbong.allmart_api.order.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.sunbong.allmart_api.order.domain.OrderEntity;
+import org.sunbong.allmart_api.order.repository.search.OrderSearch;
+
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long>, OrderSearch {
+}
