a36a34236624936130cc99fb01448b43ee3bed34
BigJins <sp04212016@gmail.com>
2024-11-08 11:16:23 +0900

    order (#29)
    fix : oder entity에 두개 중복

--- src/main/java/org/sunbong/allmart_api/order/domain/OrderEntity.java
+++ src/main/java/org/sunbong/allmart_api/order/domain/OrderEntity.java
@@ -43,8 +43,4 @@ public class OrderEntity extends BaseEntity {
     @JoinColumn(name = "paymentID", nullable = false)
     private Payment payment;
 
-    @OneToOne
-    @JoinColumn(name = "paymentID", nullable = true) // 변경
-    private Payment payment;
-
 }
