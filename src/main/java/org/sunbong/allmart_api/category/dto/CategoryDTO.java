bf97102d27c13c87778503a407a0608092e930b8
hajaemin <98006326+ha-jae-min@users.noreply.github.com>
2024-11-15 17:31:10 +0900

    #6product (#57)
    * feat: 카테고리 리스트 추가
    
    * feat: 테스트 변경
    
    * feat: 카테고리 crud 추가, dto 변경
    
    * feat: 수정시 중복 확인 안되는 거 해결

--- src/main/java/org/sunbong/allmart_api/category/dto/CategoryDTO.java
+++ src/main/java/org/sunbong/allmart_api/category/dto/CategoryDTO.java
@@ -7,7 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-public class CategoryAddDTO {
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
 
     private Long categoryID;
 
