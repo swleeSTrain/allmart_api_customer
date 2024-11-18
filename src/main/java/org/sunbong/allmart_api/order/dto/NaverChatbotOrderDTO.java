bea79128534097919de76cb5a0f810833f403048
BigJins <sp04212016@gmail.com>
2024-11-18 09:52:36 +0900

    Workspace/#11order (#62)
    * inventory 조회기능 (#47)
    
    * order
    
    fix : oder entity에 두개 중복
    
    * order
    
    refactor: OrderService 인터페이스로 만들고 Impl만들어줌.
    refactor: test코드 fake사용, 예외 클래스 만들어줌
    
    * order
    
    refactor: OrderService 인터페이스로 만들고 Impl만들어줌.
    refactor: test코드 fake사용, 예외 클래스 만들어줌햣
    
    * Workspace/employee (#30)
    
    * feat:#20employee
    -테이블추가
    -readme 수정
    
    * feat:employee
    -토큰 DTO 추가
    -readme 수정
    
    * feat:employee
    - employee수정
    -readme 수정
    
    * Workspace/#phonenumber login (#35)
    
    * [chore] 전화번호 수정
    
    * [fix] QR 코드 생성 관리 (이미지 저장)
    
    ---------
    
    Co-authored-by: seonjin <wlq862@naver.com>
    
    * feat: dev_master로 변경사항 (#36)
    
    전체적으로 수정
    
    * finish (#39)
    
    * feat: dev_master로 변경사항
    전체적으로 수정
    
    * feat: PageRequestDTO에 tag 빼고 category 필드 추가
    
    * feat: PageRequestDTO에 tag 빼고 category 필드 추가
    
    * feat Payment Point 구현
    
    * order & point (#42)
    
    fix: point 부분 PointRepository.javauserID-> customerID로 변경함.
    fix: @Primary 어노테이션을 추가하여 우선순위를 설정
    
    * Workspace/#9order (#45)
    
    * order
    
    fix : oder entity에 두개 중복
    
    * order
    
    refactor: OrderService 인터페이스로 만들고 Impl만들어줌.
    refactor: test코드 fake사용, 예외 클래스 만들어줌
    
    * order
    
    refactor: OrderService 인터페이스로 만들고 Impl만들어줌.
    refactor: test코드 fake사용, 예외 클래스 만들어줌햣
    
    * Workspace/employee (#30)
    
    * feat:#20employee
    -테이블추가
    -readme 수정
    
    * feat:employee
    -토큰 DTO 추가
    -readme 수정
    
    * feat:employee
    - employee수정
    -readme 수정
    
    * Workspace/#phonenumber login (#35)
    
    * [chore] 전화번호 수정
    
    * [fix] QR 코드 생성 관리 (이미지 저장)
    
    ---------
    
    Co-authored-by: seonjin <wlq862@naver.com>
    
    * feat: dev_master로 변경사항 (#36)
    
    전체적으로 수정
    
    * finish (#39)
    
    * feat: dev_master로 변경사항
    전체적으로 수정
    
    * feat: PageRequestDTO에 tag 빼고 category 필드 추가
    
    * feat: PageRequestDTO에 tag 빼고 category 필드 추가
    
    * feat Payment Point 구현 (#40)
    
    * order & payment
    fix: OneToOne 설정후 관계설정
    
    * order
    feature: 컨트롤러, 챗봇 json 으로 주문 생성
    fix: 리스트 순서- tuple 사용
    
    ---------
    
    Co-authored-by: STra <86188374+swleeSTrain@users.noreply.github.com>
    Co-authored-by: seonjin <wlq862@naver.com>
    Co-authored-by: hajaemin <98006326+ha-jae-min@users.noreply.github.com>
    Co-authored-by: yi9927 <114922916+yi9927@users.noreply.github.com>
    
    * workspace/#5product (#46)
    
    * feat: 카테고리 리스트 추가
    
    * feat: 테스트 변경
    
    * feat inventory 조회
    
    * feat inventory 조회
    
    ---------
    
    Co-authored-by: Daejin Kim <sp04212016@gmail.com>
    Co-authored-by: STra <86188374+swleeSTrain@users.noreply.github.com>
    Co-authored-by: seonjin <wlq862@naver.com>
    Co-authored-by: hajaemin <98006326+ha-jae-min@users.noreply.github.com>
    
    * feat: order search (#52)
    
    * fix: json파일 받아서 주문 생성 가능.
    
    * fix: git 충돌해결
    
    ---------
    
    Co-authored-by: yi9927 <114922916+yi9927@users.noreply.github.com>
    Co-authored-by: STra <86188374+swleeSTrain@users.noreply.github.com>
    Co-authored-by: seonjin <wlq862@naver.com>
    Co-authored-by: hajaemin <98006326+ha-jae-min@users.noreply.github.com>

--- src/main/java/org/sunbong/allmart_api/order/dto/NaverChatbotOrderDTO.java
+++ src/main/java/org/sunbong/allmart_api/order/dto/NaverChatbotOrderDTO.java
@@ -1,28 +1,14 @@
 package org.sunbong.allmart_api.order.dto;
 
 import lombok.Data;
+import org.sunbong.allmart_api.product.domain.Product;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class NaverChatbotOrderDTO {
     private String userId;
-    private List<Bubble> bubbles;
-
-    @Data
-    public static class Bubble {
-        private String type;
-        private DataField data;
-        private List<Slot> slot;
-
-        @Data
-        public static class DataField {
-            private String description;
-        }
-
-        @Data
-        public static class Slot {
-            private String name;
-            private String value;
-        }
-    }
+    private String productName;
+    private int quantity;
 }
