2ace19a81580729847f3b04a53af4e86fcfa5a10
hajaemin <woals3527@naver.com>
2024-11-22 09:31:34 +0900

    feat: 상품 등록시 재고 같이 등록

--- src/main/java/org/sunbong/allmart_api/config/WebConfig.java
+++ src/main/java/org/sunbong/allmart_api/config/WebConfig.java
@@ -1,9 +1,18 @@
 package org.sunbong.allmart_api.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.inject.Qualifier;
+import javax.sql.DataSource;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -25,4 +34,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///C:/upload/"); // 실제 업로드 경로
     }
+
 }
--- src/test/java/org/sunbong/allmart_api/inventory/repository/InventoryRepositoryTest.java
+++ src/test/java/org/sunbong/allmart_api/inventory/repository/InventoryRepositoryTest.java
@@ -3,6 +3,7 @@ package org.sunbong.allmart_api.inventory.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.sunbong.allmart_api.inventory.domain.Inventory;
 
@@ -18,23 +19,39 @@ class InventoryRepositoryTest {
     @Test
     void insertBulkData() {
         // 1부터 100까지의 더미 데이터를 삽입
-        IntStream.rangeClosed(1, 100).forEach(i -> {
-            Inventory inventory = Inventory.builder()
-                    .sku("TESTSKU" + i) // SKU 값을 TESTSKU1, TESTSKU2, ...로 설정
-                    .quantity(i * 10)  // 수량을 i * 10으로 설정
-                    .inStock(1)        // 항상 재고 있음으로 설정
-                    .build();
+//        IntStream.rangeClosed(1, 100).forEach(i -> {
+//            Inventory inventory = Inventory.builder()
+//                    .sku("TESTSKU" + i) // SKU 값을 TESTSKU1, TESTSKU2, ...로 설정
+//                    .quantity(i * 10)  // 수량을 i * 10으로 설정
+//                    .inStock(1)        // 항상 재고 있음으로 설정
+//                    .build();
+//
+//            inventoryRepository.save(inventory); // 데이터베이스에 저장
+//        });
+//
+//        // 데이터 저장 후 확인
+//        long count = inventoryRepository.count(); // 총 데이터 개수 확인
+//        System.out.println("Total Inventory Count: " + count);
+//
+//        // 일부 데이터를 확인
+//        inventoryRepository.findAll().stream()
+//                .limit(5) // 5개 데이터만 확인
+//                .forEach(System.out::println);
+    }
 
-            inventoryRepository.save(inventory); // 데이터베이스에 저장
-        });
-
-        // 데이터 저장 후 확인
-        long count = inventoryRepository.count(); // 총 데이터 개수 확인
-        System.out.println("Total Inventory Count: " + count);
-
-        // 일부 데이터를 확인
-        inventoryRepository.findAll().stream()
-                .limit(5) // 5개 데이터만 확인
-                .forEach(System.out::println);
+    @Test
+    void insertBulk() {
+//        Inventory inventory = Inventory.builder()
+//                .sku("TESTSKU1")  // SKU 값을 TESTSKU1로 설정
+//                .quantity(10)     // 수량을 10으로 설정
+//                .inStock(1)       // 항상 재고 있음으로 설정
+//                .build();
+//
+//        inventoryRepository.save(inventory); // 데이터베이스에 저장
+//
+//        long count = inventoryRepository.count(); // 총 데이터 개수 확인
+//        System.out.println("Total Inventory Count: " + count);
+//
+//        inventoryRepository.findAll().forEach(System.out::println);
     }
 }
