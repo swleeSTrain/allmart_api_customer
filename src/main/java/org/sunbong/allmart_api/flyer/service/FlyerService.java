726fe2358a53b66f64d02be409cfe1f11474feb1
hajaemin <woals3527@naver.com>
2024-11-24 19:40:38 +0900

    feat: 리스트, 조회 서비스 컨트롤러

--- src/main/java/org/sunbong/allmart_api/flyer/service/FlyerService.java
+++ src/main/java/org/sunbong/allmart_api/flyer/service/FlyerService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.sunbong.allmart_api.common.dto.PageRequestDTO;
+import org.sunbong.allmart_api.common.dto.PageResponseDTO;
+import org.sunbong.allmart_api.flyer.dto.FlyerListDTO;
+import org.sunbong.allmart_api.flyer.dto.FlyerReadDTO;
 import org.sunbong.allmart_api.flyer.repository.FlyerRepository;
 
 @Service
@@ -13,6 +17,23 @@ import org.sunbong.allmart_api.flyer.repository.FlyerRepository;
 public class FlyerService {
 
     private final FlyerRepository flyerRepository;
+
+    // 조회
+    public FlyerReadDTO readById(Long id) {
+
+        FlyerReadDTO result = flyerRepository.readById(id);
+
+        return result;
+    }
+
+    // 리스트
+    public PageResponseDTO<FlyerListDTO> list(PageRequestDTO pageRequestDTO) {
+
+        PageResponseDTO<FlyerListDTO> result = flyerRepository.list(pageRequestDTO);
+
+        return result;
+    }
+
 
 
 }
