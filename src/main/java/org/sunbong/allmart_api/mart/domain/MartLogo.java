ce839b384a961e4939848d4d3cb96842223007d8
hajaemin <woals3527@naver.com>
2024-11-25 16:56:12 +0900

    feat: 마트 엔티티 변경

--- src/main/java/org/sunbong/allmart_api/mart/domain/MartLogo.java
+++ src/main/java/org/sunbong/allmart_api/mart/domain/MartLogo.java
@@ -3,19 +3,12 @@ package org.sunbong.allmart_api.mart.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "tbl_martLogo") // 테이블명
-@Getter
-@Builder
+@Embeddable
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@Getter
 public class MartLogo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "logoID")
-    private Long logoID; // 마트ID (기본키)
 
     @Column(name = "logoURL", length = 255, nullable = false)
     private String logoURL; // 로고 URL
