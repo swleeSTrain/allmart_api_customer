8b61e3e5c758f5d94ba72f99d92877d747a5f32a
hajaemin <98006326+ha-jae-min@users.noreply.github.com>
2024-11-25 17:01:07 +0900

    #2mart (#92)
    * feat: 전단지 부분 세팅
    
    * feat: signIn ADD (#80)
    
    로그인 시큐리티 추가
    
    * feat: 전단지 리스트, 조회, 등록 테스트
    상품 이미지 부분 변수명 변경
    
    * feat: 상품 등록시 재고 같이 등록
    
    * feat: 소프트 딜리트로 변경중
    
    * refactor: 중복시 예외처리, 소프트딜리트로 변경
    
    * feat: 리스트, 조회 서비스 컨트롤러
    
    * merge (#85)
    
    * feat: 전단지 부분 세팅
    
    * feat: signIn ADD (#80)
    
    로그인 시큐리티 추가
    
    * feat: 전단지 리스트, 조회, 등록 테스트
    상품 이미지 부분 변수명 변경
    
    * feat: 상품 등록시 재고 같이 등록
    
    * feat: 소프트 딜리트로 변경중
    
    * refactor: 중복시 예외처리, 소프트딜리트로 변경
    
    * feat: 리스트, 조회 서비스 컨트롤러
    
    ---------
    
    Co-authored-by: STra <86188374+swleeSTrain@users.noreply.github.com>
    
    * feat 주소등록 (#88)
    
    * feat 주소등록
    
    * feat 주소등록
    
    * feat 주소등록2
    
    * feat: 마트 엔티티
    
    * feat: 마트 엔티티 변경
    
    * chore: 머지용 커밋
    
    ---------
    
    Co-authored-by: STra <86188374+swleeSTrain@users.noreply.github.com>
    Co-authored-by: yi9927 <114922916+yi9927@users.noreply.github.com>

---
+++ src/main/java/org/sunbong/allmart_api/flyer/dto/FlyerAddDTO.java
@@ -0,0 +1,13 @@
+//package org.sunbong.allmart_api.flyer.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class FlyerAddDTO {
+//}
