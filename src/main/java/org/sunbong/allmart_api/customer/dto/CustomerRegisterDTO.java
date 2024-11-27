2f3c00dbd5a6413caf6d9b6e2c81ab7a9417b75b
youngin <duddls1304@naver.com>
2024-11-27 17:59:01 +0900

    feat 주소등록목록완료.

--- src/main/java/org/sunbong/allmart_api/customer/dto/CustomerRegisterDTO.java
+++ src/main/java/org/sunbong/allmart_api/customer/dto/CustomerRegisterDTO.java
@@ -1,6 +1,5 @@
 package org.sunbong.allmart_api.customer.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,15 +9,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class CustomerListDTO {
+public class CustomerRegisterDTO {
 
-    private Long customerID;
     private String name;        // 고객 이름
     private String phoneNumber; // 고객 전화번호
     private String postcode;    // 우편번호
     private String roadAddress; // 도로명 주소
     private String detailAddress; // 상세 주소
     private String fullAddress;  // 전체 주소
-    private int loyaltyPoints;
-
 }
