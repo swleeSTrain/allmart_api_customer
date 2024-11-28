4b0aa602111c16a05d6db0913279273fd4994201
Daejin Kim <sp04212016@gmail.com>
2024-11-28 20:10:22 +0900

    #docs: 배달 컨트롤러 알아보기 쉽게 주석

--- src/main/java/org/sunbong/allmart_api/delivery/controller/DeliveryController.java
+++ src/main/java/org/sunbong/allmart_api/delivery/controller/DeliveryController.java
@@ -24,30 +24,55 @@ public class DeliveryController {
 
     private final DeliveryService deliveryService;
 
+    /**
+     * 관리자가 특정 시간 범위 동안의 주문 데이터를 처리하여 배달 작업을 생성해야 할 때 사용
+     * @param startTime
+     * @param endTime
+     * @return
+     */
     @PostMapping("/process")
     public ResponseEntity<List<DeliveryDTO>> processDelivery(@RequestParam LocalDateTime startTime,
                                                              @RequestParam LocalDateTime endTime) {
         return ResponseEntity.ok(deliveryService.processOrdersForDelivery(startTime, endTime));
     }
 
+    /**
+     * 관리자가 특정 배달 작업에 포함된 주문 목록을 조회해야 할 때 사용
+     * @param deliveryId
+     * @return
+     */
     @GetMapping("/{deliveryId}/orders")
     public ResponseEntity<List<OrderDTO>> getOrdersByDelivery(@PathVariable Long deliveryId) {
         return ResponseEntity.ok(deliveryService.getOrdersByDeliveryId(deliveryId));
     }
 
+    /**
+     * 관리자가 배달 상태를 PENDING → IN_PROGRESS 또는 COMPLETED로 변경
+     * @param deliveryId
+     * @param status 배달 상태
+     * @return
+     */
     @PutMapping("/{deliveryId}/status")
     public ResponseEntity<DeliveryDTO> changeDeliveryStatus(@PathVariable Long deliveryId,
                                                             @RequestParam String status) {
         return ResponseEntity.ok(deliveryService.changeDeliveryStatus(deliveryId, status));
     }
 
-    //대시보드 숫자
+    /**
+     * 대시보드 시각화
+     * @return
+     */
     @GetMapping("/status-count")
     public ResponseEntity<Map<String, Long>> getDeliveryStatusCount() {
         return ResponseEntity.ok(deliveryService
                 .getDeliveryStatusCount());
     }
 
+    /**
+     * 상태별 리스트 보여주기
+     * @param status 배달 상태
+     * @return
+     */
     @GetMapping("/orders-by-status")
     public ResponseEntity<List<OrderDTO>> getOrdersByStatus(@RequestParam String status) {
         return ResponseEntity.ok(deliveryService.getOrdersByStatus(status));
