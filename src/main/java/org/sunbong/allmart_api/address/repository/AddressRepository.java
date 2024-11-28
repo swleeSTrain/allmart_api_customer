73bf9ed13c908746b78ba089bf255863add29e70
yi9927 <114922916+yi9927@users.noreply.github.com>
2024-11-28 10:47:27 +0900

    merge 후 cutomer, address수정 (#99)
    * feat 주소등록
    
    * feat 주소등록
    
    * feat 주소등록2
    
    * feat 주소등록목록완료.
    
    * feat 주소등록목록완료.
    
    * feat 주소등록목록완료.
    
    * feat 주소등록목록완료.

--- src/main/java/org/sunbong/allmart_api/address/repository/AddressRepository.java
+++ src/main/java/org/sunbong/allmart_api/address/repository/AddressRepository.java
@@ -1,7 +1,12 @@
 package org.sunbong.allmart_api.address.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.sunbong.allmart_api.address.domain.Address;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    @Query("SELECT a FROM Address a JOIN FETCH a.customer")
+    List<Address> findAllWithCustomer();
 }
