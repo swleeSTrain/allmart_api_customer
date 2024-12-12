0eb0beb1c87c1020fd34f0a9325c2dbe8946ea96
seonjin <wlq862@naver.com>
2024-12-12 18:42:40 +0900

    pull을 위한

--- src/main/java/org/sunbong/allmart_api/social/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/org/sunbong/allmart_api/social/config/CustomAuthenticationFailureHandler.java
@@ -1,12 +1,12 @@
 package org.sunbong.allmart_api.social.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
