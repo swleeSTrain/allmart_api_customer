6ccd05ef50f796895810e32ad94d0626a90fa212
hajaemin <woals3527@naver.com>
2024-12-12 11:29:33 +0900

    chore: 바꿈

--- src/main/java/org/sunbong/allmart_api/config/QuerydslConfig.java
+++ src/main/java/org/sunbong/allmart_api/config/QuerydslConfig.java
@@ -2,15 +2,17 @@ package org.sunbong.allmart_api.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QuerydslConfig {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
+
+    public QuerydslConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
--- src/main/java/org/sunbong/allmart_api/security/auth/CustomuserPrincipal.java
+++ src/main/java/org/sunbong/allmart_api/security/auth/CustomuserPrincipal.java
@@ -3,22 +3,15 @@ package org.sunbong.allmart_api.security.auth;
 import java.security.Principal;
 
 public class CustomuserPrincipal implements Principal {
-    private String email;
-    private String role;
-    private String phone;
-    private String loginType;
 
-    public CustomuserPrincipal(String email, String role, String phone, String loginType) {
+    private final String email;
+
+    public CustomuserPrincipal(final String email) {
         this.email = email;
-        this.role = role;
-        this.phone = phone;
-        this.loginType = loginType;
     }
 
     @Override
     public String getName() {
-        return "";
+        return email;
     }
-
-    // Getters and Setters
 }
